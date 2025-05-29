e570735819a70c6fe910b520d06206b2e54fcce0
Alexander Nguyen <anguyen412@ucsb.edu>
2025-05-28 18:46:41 -0700

    update cost function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ define_language! {
         "+" = Add([Id; 2]),
         "*" = Mul([Id; 2]),
         "-" = Sub([Id; 2]),
+        "constmul" = ConstMul([Id; 2]),
         "square" = Square(Id),
+        Const(u64),
         Symbol(Symbol),
     }
 }
@@ -22,12 +24,25 @@ impl CostFunction<Fp4Expr> for UnitCost {
         let base_cost = match enode {
             Fp4Expr::Add(_) => 1,      // cost for "+"
             Fp4Expr::Mul(_) => 10,      // cost for "*"
+            Fp4Expr::ConstMul(_) => 4,
             Fp4Expr::Sub(_) => 1,      // cost for "-"
             Fp4Expr::Square(_) => 6,   // cost for "square"
             Fp4Expr::Symbol(_) => 0,   // no cost for symbols (variables/constants)
+            Fp4Expr::Const(_) => 0,
         };
 
         base_cost + enode.children().iter().map(|&id| costs(id)).sum::<usize>()
+    }
+}
+
+fn is_const(var: &str) -> impl Fn(&mut EGraph<Fp4Expr, ()>, Id, &Subst) -> bool {
+    let var_id = var.parse().unwrap();
+    move |egraph, _, subst| {
+        // Get the eclass id for the variable
+        let eclass_id = subst[var_id];
+
+        // Check if the eclass contains a Const node
+        egraph[eclass_id].nodes.iter().any(|node| matches!(node, Fp4Expr::Const(_)))
     }
 }
 
@@ -35,9 +50,15 @@ fn main() {
     let rules: &[Rewrite<Fp4Expr, ()>] = &[
         rw!("square_add"; "(square (+ ?a ?b))" => "(+ (+ (square ?a) (square ?b)) (* 2 (* ?a ?b)))"),
         rw!("mul_const"; "(* 2 ?a)" => "(+ ?a ?a)"),
+        rw!("mulsquare"; "(* ?x ?x)" => "(square ?x)"),
+        rw!(
+            "two_xy_to_squares";
+            "(* 2 (* ?x ?y))" => "(- (square (+ ?x ?y)) (+ (square ?x) (square ?y)))" // 2xy = (x+y)^2 - x^2 - y^2
+        ),
+        rw!("mul_to_constmul"; "(* ?a ?b)" => "(constmul ?a ?b)" if is_const("?a")),
     ];
 
-    let expr = "(* 2 a0)"; // ξ
+    let expr = "(* 3 x)";
     let expr: RecExpr<Fp4Expr> = expr.parse().unwrap();
 
     let mut egraph = EGraph::default();
