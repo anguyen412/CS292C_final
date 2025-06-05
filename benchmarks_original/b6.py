ac26ea673a47e15c0335ee00c9a8e8a350d24d80
jtmnguyen <jessicanguyen@ucsb.edu>
2025-06-04 22:47:16 -0700

    clean up all benchmark files

--- benchmarks_original/b6.py
+++
@@ -1,5 +0,0 @@
-def easy_part_naive(p):       # repeated Frobenius & mul
-    return p ** ((p**4 - p**2 + 1))
-
-# Compressed method (Karabina) with fewer Fp⁶ muls
-# Students synthesize series of Frobenius maps + squarings
